ff5ea6981d1f1d7380030192049ab717849d2980
Stanislau Volkau <svolkau@c4r1s6.42warsaw.pl>
2025-06-16 12:20:28 +0200

    42

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: svolkau <gvardovski@icloud.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 09:17:17 by svolkau           #+#    #+#             */
-/*   Updated: 2025/06/09 14:42:13 by svolkau          ###   ########.fr       */
+/*   Updated: 2025/06/16 12:18:40 by svolkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,16 @@ void	printarr(char **gv, int start, int mes)
 		ft_printf("\n");
 }
 
+void checkmnflag(char **gv, int i, int len)
+{
+	if (ft_strncmp("-n", gv[i], len) == 0 && (arr_len(gv) - 1) == i)
+		ft_printf("");
+	if (ft_strncmp("-n", gv[i], len) == 0)
+		printarr(gv, i + 1, 1);
+	if (ft_strncmp("-n", gv[i], len) != 0)
+		printarr(gv, i, 0);
+}
+
 int	ft_echo(t_main_dat *main_data, char **gv)
 {
 	t_main_dat	temp;
@@ -62,11 +72,8 @@ int	ft_echo(t_main_dat *main_data, char **gv)
 		i++;
 	}
 	len = ft_strlen(gv[i]);
-	if (ft_strncmp("-n", gv[i], len) == 0 && (arr_len(gv) - 1) == i)
-		ft_printf("");
-	if (ft_strncmp("-n", gv[i], len) == 0)
-		printarr(gv, i + 1, 1);
-	if (ft_strncmp("-n", gv[i], len) != 0)
-		printarr(gv, i, 0);
+	if (!len)
+		len = 1;
+	checkmnflag(gv, i, len);
 	return (0);
 }
